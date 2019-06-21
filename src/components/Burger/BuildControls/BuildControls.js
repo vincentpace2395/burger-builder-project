45a34639539f6978479f3feb82a22ee80c5780ae
Vincent Pace <vpace@nhpace.net>
2019-06-20 21:50:42 -0500

    Add ability to remove ingredients and display price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,11 +12,14 @@ const controls = [
 const buildControls = props => {
     return (
         <div className='BuildControls'>
+            <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
             {controls.map(ctrl => {
                 return <BuildControl
                     key={ctrl.label}
                     label={ctrl.label}
                     added={() => props.ingredientAdded(ctrl.type)}
+                    removed={() => props.ingredientRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]}
                 />
             })}
         </div>
